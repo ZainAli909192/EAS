d7671a653141e9cd13ff1ba0ab9483cffa7c0fc4
Zain Ali <malikzain909192@gmail.com>
2025-03-24 13:25:27 +0200

    why chose us, process etc added

--- js/script.js
+++ js/script.js
@@ -275,3 +275,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// slider js 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// new slider 
